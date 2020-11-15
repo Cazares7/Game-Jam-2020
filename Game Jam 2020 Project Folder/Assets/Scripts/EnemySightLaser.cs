56d2d07229ed396c950584ce5b1661f9c6c0f2a2
Cazares7 <43894633+Cazares7@users.noreply.github.com>
2020-11-14 22:26:05 -0800

    Minor Fixes

---
+++ Game Jam 2020 Project Folder/Assets/Scripts/EnemySightLaser.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class EnemySightLaser : MonoBehaviour
+{
+    public LineRenderer lineOfSight;
+    public float range;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    { 
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.right, range);
+
+
+
+
+        if (hitinfo.collider != null)
+        {
+
+            Debug.Log("Pooooo");
+            lineOfSight.SetPosition(1, hitinfo.point);
+
+            if (hitinfo.collider.CompareTag("Player")) {
+                CustomEvent.Trigger(GameObject.FindGameObjectWithTag("Player"), "Death");
+            }
+
+        }
+        else
+        {
+            Debug.Log("Po");
+            lineOfSight.SetPosition(1, transform.position + transform.right * range);
+
+        }
+        lineOfSight.SetPosition(0, transform.position);
+
+
+
+
+
+   
+    
+    }
+
+   
+}
