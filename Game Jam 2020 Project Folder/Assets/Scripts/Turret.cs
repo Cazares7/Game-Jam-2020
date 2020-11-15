f7908bc6f7e5cb472715edd8048889f138efe826
Cazares7 <43894633+Cazares7@users.noreply.github.com>
2020-11-15 02:35:07 -0800

    Turrets now working

---
+++ Game Jam 2020 Project Folder/Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform shootPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject Shoot(GameObject bullet) {
+      GameObject shot =  Instantiate(bullet, shootPoint.position, Quaternion.identity);
+      return shot;
+    }
+}
