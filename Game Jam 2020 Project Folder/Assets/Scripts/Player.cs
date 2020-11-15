3d4f663efadd056f2a3e98baa09c731d5eaf03f1
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2020-11-14 16:33:27 -0800

    Added Movement & Lighting Switching to Player

---
+++ Game Jam 2020 Project Folder/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+
+    public bool inLight = false;
+    
+    public float lightDuration = 2.0f;
+    public float lightCooldownDuration = 2.0f;
+    
+
+    private Rigidbody2D body;
+
+    private float lightCooldownTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!inLight)
+        {
+            if (lightCooldownTime >= lightCooldownDuration)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    StartCoroutine(InLightMode());
+                }
+            }
+            else
+            {
+                lightCooldownTime += Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator InLightMode()
+    {
+        lightCooldownTime = 0.0f;
+
+        inLight = true;
+        yield return new WaitForSeconds(lightDuration);
+        inLight = false;
+    }
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        body.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
